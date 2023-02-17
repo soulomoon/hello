383a5f02266c1e71b23709d2c7e88ba6e55fa80a
Patrick <fwy996602672@gmail.com>
2023-02-17 20:59:50 +0800

    remove explicit system

--- flake.nix
+++ flake.nix
@@ -3,31 +3,33 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/cfa78cb43389635df0a9086cb31b74d3c3693935";
 
-  outputs = { self, nixpkgs, flake-utils}: 
-       flake-utils.lib.eachDefaultSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        rec { packages = 
-              { hello = pkgs.stdenv.mkDerivation {
-                          pname = "hello-repeater";
-                          version = "1.0.0";
-                          # src = pkgs.fetchgit {
-                          #   url = "https://github.com/soulomoon/hello.git";
-                          #   # rev = "c++-code-alone";
-                          #   sha256 = "sha256-oT6HLkccIfhMX4b0Hk85NHD/WONnrq6OYpKuDsa63aY=";
-                          # };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        packages =
+          {
+            hello = pkgs.stdenv.mkDerivation {
+              pname = "hello-repeater";
+              version = "1.0.0";
+              # src = pkgs.fetchgit {
+              #   url = "https://github.com/soulomoon/hello.git";
+              #   # rev = "c++-code-alone";
+              #   sha256 = "sha256-oT6HLkccIfhMX4b0Hk85NHD/WONnrq6OYpKuDsa63aY=";
+              # };
 
-                          src = ./.;
-                          installPhase = ''
-                            mkdir -p $out/bin
-                            cp hello_cmake $out/bin/hello-repeater
-                          '';
+              src = ./.;
+              installPhase = ''
+                mkdir -p $out/bin
+                cp hello_cmake $out/bin/hello-repeater
+              '';
 
-                          nativeBuildInputs = [
-                            pkgs.cmake
-                          ]; 
-                          };
-                default = self.packages.${system}.hello;
-              };
-           }
-      );
+              nativeBuildInputs = [
+                pkgs.cmake
+              ];
+            };
+            default = packages.hello;
+          };
+      }
+    );
 }
