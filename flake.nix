e24402243894ec152131c5a12a70462c8719443f
Patrick <fwy996602672@gmail.com>
2023-02-17 19:53:31 +0800

    use local src

--- flake.nix
+++ flake.nix
@@ -9,11 +9,13 @@
           in pkgs.stdenv.mkDerivation {
               pname = "hello-repeater";
               version = "1.0.0";
-              src = pkgs.fetchgit {
-                url = "https://github.com/soulomoon/hello.git";
-                # rev = "c++-code-alone";
-                sha256 = "sha256-oT6HLkccIfhMX4b0Hk85NHD/WONnrq6OYpKuDsa63aY=";
-              };
+              # src = pkgs.fetchgit {
+              #   url = "https://github.com/soulomoon/hello.git";
+              #   # rev = "c++-code-alone";
+              #   sha256 = "sha256-oT6HLkccIfhMX4b0Hk85NHD/WONnrq6OYpKuDsa63aY=";
+              # };
+
+              src = ./.;
               installPhase = ''
                 mkdir -p $out/bin
                 cp hello_cmake $out/bin/hello-repeater
